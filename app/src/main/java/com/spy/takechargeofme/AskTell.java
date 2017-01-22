796f98b1649755c65283537e2587de5ad2a41a71
Ramez <rameznahas@gmail.com>
2017-01-21 21:09:44 -0500

    Ask Tell, first push

---
+++ app/src/main/java/com/spy/takechargeofme/AskTell.java
@@ -0,0 +1,13 @@
+package com.spy.takechargeofme;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AskTell extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ask_tell);
+    }
+}
