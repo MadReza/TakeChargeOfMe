51edc0961fdcc520804b575e0cb3d62abe201eef
Chris <christopher.yazbek@gmail.com>
2017-01-22 00:43:16 -0500

    this shit

--- app/src/main/java/com/spy/takechargeofme/MainActivity.java
+++ app/src/main/java/com/spy/takechargeofme/MainActivity.java
@@ -2,12 +2,35 @@ package com.spy.takechargeofme;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.telephony.SmsManager;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        Button Send;
+        EditText phoneNumber;
+        EditText msgText;
+
+        @Override
+        protected void onCreate (Bundle savedInstanceState){
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.activity_main);
+
+            phoneNumber = (EditText) findViewById(R.id.phoneNum);
+            msgText = (EditText) findViewById(R.id.msgField);
+
+            Send = (Button) findViewById(R.id.smsButton);
+            Send.setOnClickListener(new View.OnClickListener() {
+                public void onClick(View v) {
+
+                    sendSMS(phoneNumber.getText().toString(), msgText.getText().toString());
+                }
+            });
+        }
+    public void sendSMS (String phoneNb, String msgText) {
+        SmsManager sms = SmsManager.getDefault();
+        sms.sendTextMessage(phoneNb, "5145693080", msgText, null, null);
     }
 }
