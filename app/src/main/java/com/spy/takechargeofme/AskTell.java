716ffe1ee0ca3eef50ddf08258e497fd045a1c91
Ramez <rameznahas@gmail.com>
2017-01-21 21:35:33 -0500

    Ask Tell, linked Contact Picture and Contact Name

--- app/src/main/java/com/spy/takechargeofme/AskTell.java
+++ app/src/main/java/com/spy/takechargeofme/AskTell.java
@@ -2,12 +2,22 @@ package com.spy.takechargeofme;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.ImageView;
 
 public class AskTell extends AppCompatActivity {
+
+    ImageView contactPicture;
+    EditText contactName;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_ask_tell);
+        contactPicture = (ImageView)findViewById(R.id.contactPicture);
+        contactName = (EditText)findViewById(R.id.contactName);
+
+        contactName.setText("Ramez");
     }
 }
