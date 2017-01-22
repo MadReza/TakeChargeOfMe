6f4fc4ea925bf1286855573a0b5076b5c193cb9d
Chris <christopher.yazbek@gmail.com>
2017-01-22 04:14:08 -0500

    Bravo Ramzou

--- app/src/main/java/com/spy/takechargeofme/ContactList.java
+++ app/src/main/java/com/spy/takechargeofme/ContactList.java
@@ -6,14 +6,91 @@ import android.database.Cursor;
 import android.provider.ContactsContract;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
 
 
 public class ContactList extends AppCompatActivity {
 
+    public ArrayList<Kontakts> KKList;
+    ArrayList<String> movies;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_contact_list);
+
+        // Get the reference of movies
+        ListView moviesList=(ListView)findViewById(R.id.contacts_list);
+
+        KKList = new ArrayList<Kontakts>();
+        movies = new ArrayList<String>();
+        getMovies();
+
+        // Create The Adapter with passing ArrayList as 3rd parameter
+        ArrayAdapter<String> arrayAdapter =
+                new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1, movies);
+        // Set The Adapter
+        moviesList.setAdapter(arrayAdapter);
+
+        // register onClickListener to handle click events on each item
+        moviesList.setOnItemClickListener(new AdapterView.OnItemClickListener()
+        {
+            // argument position gives the index of item which is clicked
+            public void onItemClick(AdapterView<?> arg0, View v, int position, long arg3)
+            {
+                String selectedmovie=movies.get(position);
+                Toast.makeText(getApplicationContext(), "Movie Selected : "+selectedmovie,   Toast.LENGTH_LONG).show();
+            }
+        });
+    }
+
+    public void getMovies()
+    {
+        ContentResolver cr = getContentResolver();
+        Cursor cur = cr.query(ContactsContract.Contacts.CONTENT_URI,
+                null, null, null, null);
+
+        if (cur.getCount() > 0) {
+            while (cur.moveToNext()) {
+                String id = cur.getString(
+                        cur.getColumnIndex(ContactsContract.Contacts._ID));
+                String name = cur.getString(cur.getColumnIndex(
+                        ContactsContract.Contacts.DISPLAY_NAME));
+
+
+                if (cur.getInt(cur.getColumnIndex(
+                        ContactsContract.Contacts.HAS_PHONE_NUMBER)) > 0) {
+                    Cursor pCur = cr.query(
+                            ContactsContract.CommonDataKinds.Phone.CONTENT_URI,
+                            null,
+                            ContactsContract.CommonDataKinds.Phone.CONTACT_ID +" = ?",
+                            new String[]{id}, null);
+                    while (pCur.moveToNext()) {
+                        String phoneNo = pCur.getString(pCur.getColumnIndex(
+                                ContactsContract.CommonDataKinds.Phone.NUMBER));
+                        KKList.add(new Kontakts(name, phoneNo));
+                    }
+                    pCur.close();
+                }
+            }
+            for(int i = 0 ; i < KKList.size(); i++){
+                movies.add(KKList.get(i).getName());
+            }
+        }
+    }
+
+
+
+
+
+        /*
+
         setContentView(R.layout.activity_contact_list);
 
         ContentResolver cr = getContentResolver();
@@ -27,6 +104,7 @@ public class ContactList extends AppCompatActivity {
                 String name = cur.getString(cur.getColumnIndex(
                         ContactsContract.Contacts.DISPLAY_NAME));
 
+
                 if (cur.getInt(cur.getColumnIndex(
                         ContactsContract.Contacts.HAS_PHONE_NUMBER)) > 0) {
                     Cursor pCur = cr.query(
@@ -37,13 +115,13 @@ public class ContactList extends AppCompatActivity {
                     while (pCur.moveToNext()) {
                         String phoneNo = pCur.getString(pCur.getColumnIndex(
                                 ContactsContract.CommonDataKinds.Phone.NUMBER));
-                        Toast.makeText(ContactList.this, "Name: " + name
-                                + ", Phone No: " + phoneNo, Toast.LENGTH_SHORT).show();
+                        KKList.add(new Kontakts(name, phoneNo));
                     }
                     pCur.close();
                 }
             }
+            Toast.makeText(ContactList.this, KKList.size()+"", Toast.LENGTH_SHORT).show();
         }
-    }
+    }*/
 
-    }
+}
--- app/src/main/java/com/spy/takechargeofme/Kontakts.java
+++ app/src/main/java/com/spy/takechargeofme/Kontakts.java
@@ -5,6 +5,30 @@ package com.spy.takechargeofme;
  */
 
 public class Kontakts {
+
     private String name;
     private String number;
+
+    public Kontakts(String name, String number){
+        this.name = name;
+        this.number = number;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setNumber(String number) {
+        this.number = number;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getNumber() {
+        return number;
+    }
+
+
 }
